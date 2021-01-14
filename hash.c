1cf10b4baa94fd532003cab04100fbd945eea8d2
Nadav Raz <nadav@logan.sparks.io>
2021-01-14 21:23:55 +0200

    hash function indent fix

--- hash.c
+++ hash.c
@@ -1,13 +1,13 @@
 unsigned long hash(unsigned long id, int arraySize) {
-		// Starting point.
-        unsigned long hash = 5381;
-		unsigned long digit;
+	// Starting point.
+	unsigned long hash = 5381;
+	unsigned long digit;
 
-        while (id != 0) {
-			digit = id % 10;
-            hash = ((hash << 5) + hash) + digit; /* hash * 33 + c */
-			id = id / 10;
-		}
-        return hash % arraySize;
+	while (id != 0) {
+		digit = id % 10;
+		hash = ((hash << 5) + hash) + digit; /* hash * 33 + c */
+		id = id / 10;
+	}
+	return hash % arraySize;
 
 }
