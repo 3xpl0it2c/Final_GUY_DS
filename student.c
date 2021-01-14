67df91e6ff921eb4b67b6b7ccc7f903685159507
Nadav Raz <nadav@logan.sparks.io>
2021-01-14 21:55:18 +0200

    fix student headers

--- student.c
+++ student.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "student.h"
 
-typedef struct Student student_type;
 
 student_type* newStudent(unsigned long studentID) {
 	// Ask the computer to give us memory for a student.
--- student.h
+++ student.h
@@ -1,11 +1,14 @@
 struct Course {
 	int id;
 	int grade;
-}
+};
 
 struct Student {
 	unsigned long id;
 	struct Course *courses;
 	int coursesNum;
 	int heapPosition;
-}
+};
+
+typedef struct Course course_type;
+typedef struct Student student_type;
