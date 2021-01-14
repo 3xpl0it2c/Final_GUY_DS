136c6d9871048e34a242a30fc3a48efce4ddf558
Nadav Raz <nadav@logan.sparks.io>
2021-01-14 21:14:54 +0200

    hash algorithm

---
+++ hash.c
@@ -0,0 +1,12 @@
+unsigned long hash(unsigned long id, int arraySize) {
+		// Starting point.
+        unsigned long hash = 5381;
+
+        while (id != 0) {
+			digit = id % 10;
+            hash = ((hash << 5) + hash) + digit; /* hash * 33 + c */
+			id = id / 10;
+		}
+        return hash % arraySize;
+
+}
